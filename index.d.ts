5f0f643e8f68ee74d4796a94ea5e2cb6991a63f2
Brian <brian@amprobotics.com>
2023-10-24 15:04:24 -0600

    remove the import and make it JUST STRINGIFY

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,2 @@
 import { stringify } from './base.js';
 export { stringify }
-
-// export * as default from './base.js';
-
-// Workaround for TS missing feature.
-// import * as queryString from './base.js';
-
-// export default queryString;
-
-// export {
-// 	type ParseOptions,
-// 	type ParsedQuery,
-// 	type ParsedUrl,
-// 	type StringifyOptions,
-// 	type Stringifiable,
-// 	type StringifiableRecord,
-// 	type UrlObject,
-// } from './base.js';
